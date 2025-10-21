0b890f014ce93bcbcdbc26b81af31ca0b5466d80
pacatrad <pacatrad@pacatrad.fr>
2025-10-21 05:46:44 +0200

    manifest.js
    PACATRAD pwa file

---
+++ manifest.js
@@ -0,0 +1,51 @@
+{
+  "background_color": "#22bccd",
+  "description": "Le complice linguistique en ligne pour les traductions, la bureaucratie en Espagne et plus encore...",
+  "dir": "ltr",
+  "display": "standalone",
+  "name": "pacatrad",
+  "orientation": "any",
+  "scope": "/",
+  "short_name": "pacatrad",
+  "start_url": "/",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "https://ca58e806b8.clvaw-cdnwnd.com/626183a675882d9d7b28679c9451303d/200002576-c8fe2c8fe5/paca-800x494.ico?ph=ca58e806b8",
+      "isSquare": false,
+      "isPng": false,
+      "isWebp": false,
+      "isJpg": false,
+      "width": 0,
+      "is512x512": false,
+      "is256x256": false,
+      "is192x192": false
+    },
+    {
+      "src": "https://ca58e806b8.clvaw-cdnwnd.com/626183a675882d9d7b28679c9451303d/200003022-c3e8ac3ec8/pacatradexpress-home.webp?ph=ca58e806b8",
+      "type": "image/webp",
+      "sizes": "1024x824",
+      "isSquare": false,
+      "isPng": false,
+      "isWebp": true,
+      "isJpg": false,
+      "width": 1024,
+      "is512x512": false,
+      "is256x256": false,
+      "is192x192": false
+    },
+    {
+      "src": "https://www.pacatrad.fr/favicon.ico",
+      "type": "image/x-icon",
+      "isSquare": false,
+      "isPng": false,
+      "isWebp": false,
+      "isJpg": false,
+      "width": 0,
+      "is512x512": false,
+      "is256x256": false,
+      "is192x192": false
+    }
+  ],
+  "id": "PACATRAD EXPRESS"
+}
